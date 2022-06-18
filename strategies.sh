6123e39b6c1fd8f116b088d4d9830a029b57498d
Max R. P. Grossmann <m@max.pm>
2022-06-18 08:43:38 +0200

    Don't print header, but most popular strategies

--- strategies.sh
+++ strategies.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 python3 abm_strategies.py 0.05 20 "$(cat seed.txt)" > strategies.csv
-cut -d, -f4- < strategies.csv | sort | uniq -c | sort -rn
+grep -v welfare strategies.csv | cut -d, -f4- | sort | uniq -c | sort -rn | head
